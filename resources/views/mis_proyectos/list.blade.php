b8b6fa173b113684e25dc7612d886b39bff1cd36
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-12-01 19:03:53 -0430

    table new

--- resources/views/mis_proyectos/list.blade.php
+++ resources/views/mis_proyectos/list.blade.php
@@ -1,7 +1,25 @@
 @extends('base-admin')
 
-@section('content')
+@section('css')
+	<link href="{{ asset('/thema/admin/html/assets/plugins/DataTables/media/css/dataTables.bootstrap.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/admin/html/assets/plugins/DataTables/extensions/Responsive/css/responsive.bootstrap.min.css') }}" rel="stylesheet" />
+@endsection
 
+@section('js')
+	<script src="{{ asset('/thema/admin/html/assets/plugins/DataTables/media/js/jquery.dataTables.js') }}"></script>
+	<script src="{{ asset('/thema/admin/html/assets/plugins/DataTables/media/js/dataTables.bootstrap.min.js') }}"></script>
+	<script src="{{ asset('/thema/admin/html/assets/plugins/DataTables/extensions/Responsive/js/dataTables.responsive.min.js') }}"></script>
+	<script src="{{ asset('/thema/admin/html/assets/js/table-manage-responsive.demo.min.js') }}"></script>
+	<script>
+		$(document).ready(function() {
+			TableManageResponsive.init();
+		});
+	</script>
+@endsection
+
+
+
+@section('content')
 
 <div id="page-container" class="fade page-sidebar-fixed page-header-fixed" ng-controller="ProyectoController">
 	
@@ -47,69 +65,48 @@
 						@include('alerts.mensaje_error')
 								
 						<div ng-init = "proyectos = {{$proyectos}}"></div>
-						<div class="table-responsive">
-							<table class="table table-hover">
-							    <thead>
-							      <tr>
-							        <th>
-							        	<a href="#" ng-click="changeSort('index')">#</a>
-							        </th>		      	
-							        <th>
-							        	<a href="#" ng-click="changeSort('nombre_proyecto')">Proyecto</a>
-							        </th>
-							        <th>
-							        	<a href="#" ng-click="changeSort('nombre_cliente')">Cliente</a>
-							        </th>
-							        <th>
-							        	<a href="#" ng-click="changeSort('nombre_dominio')">Dominio</a>
-							        </th>
-							        <th>
-							        	<a href="#" ng-click="changeSort('fecha_creacion_avance')">Ultimo avance</a>
-							        </th>
-							        <th>
-							        	<a href="#" ng-click="changeSort('tipo_nombre')">Rol</a>
-							        </th>		        
-							        <th>
-							        	<a href="#" ng-click="changeSort('nombre_etapa')">Estatus</a>
-							        </th>
-
-
-
-							        <th>Operaciones</th>
-							      </tr>
-							    </thead>
-							    <tbody>
-
-							    	<tr ng-repeat="proyecto in proyectos| filter:opciones.buscador | orderBy:sort:reverse  track by $index">
-										<td>[[$index+1]]</td>
-										<td>[[proyecto.nombre_proyecto]]</td>
-										<td >
-											<a href="{{url('/clientes/[[proyecto.id_cliente]]')}}">
+                    	
+                    	<table id="data-table" class="table table-striped table-bordered nowrap" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Proyecto</th>
+                                        <th>Platform(s)</th>
+                                        <th>Cliente</th>
+                                        <th>Dominio</th>
+                                        <th>Ultimo avance</th>
+                                        <th>Rol</th>
+                                        <th>Estatus</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="odd gradeX" ng-repeat="proyecto in proyectos| filter:opciones.buscador | orderBy:sort:reverse  track by $index">
+                                        <td>[[$index+1]]</td>
+                                        <td>[[proyecto.nombre_proyecto]]</td>
+                                        <td>
+                                        	<a href="{{url('/clientes/[[proyecto.id_cliente]]')}}">
 												[[proyecto.nombre_cliente | noAsignado]]
 											</a>
 										</td>
-										<td>
-											<a href="{{url('/dominios/[[proyecto.id_dominio]]')}}">
+                                        <td>
+                                        	<a href="{{url('/dominios/[[proyecto.id_dominio]]')}}">
 												[[proyecto.nombre_dominio | noAsignado ]]
 											</a>
-										</td>
-										<td>[[proyecto.fecha_creacion_avance | DateForHumans]]</td>
-										<td>[[proyecto.nombre_tipo]]</td>
-										<td>[[proyecto.nombre_etapa]]</td>
-							        	<td >
-							        		<div class="row">
+                                        </td>
+                                        <td>[[proyecto.fecha_creacion_avance | DateForHumans]]</td>
+                                        <td>[[proyecto.nombre_tipo]]</td>
+                                        <td>[[proyecto.nombre_etapa]]</td>
+                                        <td>
+                                        	<div class="row">
 							        			<div class="box-button">
 									        		<a class="btn btn-sm btn-info btn-custon" href="{{ url( '/mis-proyectos/[[proyecto.id_proyecto]]' ) }}" data-toggle="tooltip" data-title="Detalle"><i class="fa fa-list"></i></a>
 									        		<a class="btn btn-sm btn-success btn-custon" href="{{ url( '/mis-proyectos/avances/[[proyecto.id_proyecto]]/create' ) }}" data-toggle="tooltip" data-title="Crear Avance"><i class="fa fa-line-chart"></i></a>
 							        			</div>
 							        		</div>
-							        	</td>
-								        </tr>
-
-							    </tbody>
-							</table>
-						</div>
-                    
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                     </div><!-- boby -->
                 </div>
             </div>
