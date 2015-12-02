25ffa51473820470d6fb4067ab9ccafb2ec98a35
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-12-02 18:21:10 -0430

    tipo d proyecto

--- resources/views/tipo_proyectos/create.blade.php
+++ resources/views/tipo_proyectos/create.blade.php
@@ -1,23 +1,61 @@
-@extends('layouts.base')
+@extends('base-admin')
 
 @section('content')
-	<div class="container" ng-controller="GrupoEtapasController">
 
-		<h2>Cear Etapas</h2>
-		<form action="{{ url('tipo_proyectos/') }}" method="POST">		
+<div id="page-container" class="fade page-sidebar-fixed page-header-fixed" ng-controller="GrupoEtapasController">
+	
+	@include('layouts/navbar-admin')
 
-			<br><br>
-			<div class="from-group">
-				<label for="">Identificador de proyectos</label>
-				<input type="text" class="form-control" ng-model="tipo_proyecto.nombre_tipo_proyecto" name="nombre_tipo_proyecto">
-			</div>
-			<br>
-			<br>
-					
+    @include('layouts/sidebar-admin')
+	
+	<div id="content" class="content ng-scope">
+        
+        <h1 class="page-header"><i class="fa fa-laptop"></i> Crear Tipo de Proyecto </h1>
+        
+        <div class="row">
+            <!-- begin col-12 -->
+            <div class="col-12 ui-sortable">
+                <!-- begin panel -->
+                <div class="panel panel-inverse">
+                    <div class="panel-heading">
+                        <div class="panel-heading-btn">
+                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand" data-original-title="" title=""><i class="fa fa-expand"></i></a>
+                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload" data-original-title="" title=""><i class="fa fa-repeat"></i></a>
+                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse" data-original-title="" title=""><i class="fa fa-minus"></i></a>
+                        </div>
+                        <h4 class="panel-title">Tipo de Proyectos</h4>
+                    </div>
 
-			<button type="submit">
-				Registrar
-			</button>
-		</form>
-	</div>
-@stop
+                    <div class="panel-body">
+						
+						<br><br>
+
+						<form class="form-horizontal" action="{{ url('tipo_proyectos/') }}" method="POST">		
+	
+							<div class="form-group">
+                                <label class="col-md-4 control-label">Identificador de proyectos</label>
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" ng-model="tipo_proyecto.nombre_tipo_proyecto" name="nombre_tipo_proyecto">
+                                </div>
+                            </div>									
+
+                            <br>
+
+							<center>
+								<button class="btn btn-success m-r-5 m-b-5" type="submit">
+									Registrar <i class="fa fa-pencil-square-o"></i>
+								</button>
+							</center>
+
+						</form>
+
+					</div><!-- boby -->
+                </div>
+            </div>
+        </div>
+
+    </div><!-- content -->
+	
+</div>
+@endsection
+
--- resources/views/tipo_proyectos/list.blade.php
+++ resources/views/tipo_proyectos/list.blade.php
@@ -1,37 +1,53 @@
-@extends('layouts.base')
+@extends('base-admin')
 
 @section('content')
-	<div class="container">
 
+<div id="page-container" class="fade page-sidebar-fixed page-header-fixed" ng-controller="ProyectoController">
+	
+	@include('layouts/navbar-admin')
+
+    @include('layouts/sidebar-admin')
+	
+	<div id="content" class="content ng-scope">
+        
+        <ol class="breadcrumb pull-right">
+            <div class="btn-toolbar">
+                <div class="btn-group">
+                    <a href="{{ url( '/tipo_proyectos/create' ) }}" class="btn btn-success btn-sm p-l-20 p-r-20" data-toggle="tooltip" data-title="Agregar tipo de Proyecto">
+                        <i class="fa fa-plus"></i>
+                    </a>
+                </div>
+            </div>
+        </ol>
+        
+
+        <h1 class="page-header"><i class="fa fa-sitemap"></i> Tipos de Proyecto </h1>
+		
 		@include('alerts.mensaje_error')
 		@include('alerts.mensaje_success')
 
-		<div class="row">
-			<div class="col-md-8"> <h2>Lista de grupos de etapas</h2></div>
-			<div class="col-md-4">
-				<a class="btn btn-sm btn-success" href="{{ url( '/tipo_proyectos/create' ) }}"> Agregar</a>
+        <div class="row">
+			<!-- begin col-3 -->
+			@foreach($tipo_proyectos as $tipo_proyecto)
+			<div class="col-md-3 col-sm-6">
+				<div class="widget widget-stats bg-blue">
+					<div class="stats-icon"><i class="fa fa-sitemap"></i></div>
+					<div class="stats-info">
+						<h4>PROYECTO</h4>
+						<p>{{$tipo_proyecto->nombre_tipo_proyecto}}</p>	
+					</div>
+					<div class="stats-link">
+						<a href="javascript:;">Detalle <i class="fa fa-arrow-circle-o-right"></i></a>
+					</div>
+				</div>
 			</div>
+			@endforeach
+			<!-- end col-3 -->
 
+			<div align="center">{!! $tipo_proyectos->render() !!}</div>
 		</div>
-		<br>
-		<br>
 
-		<table class="table table-hover">
-		    <thead>
-		      <tr>
-		        <th>Nombre Tipo Proyecto</th>
-		      </tr>
-		    </thead>
-		    <tbody>
-		    	@foreach($tipo_proyectos as $tipo_proyecto)
-			    	<tr>
-						<td>{{$tipo_proyecto->nombre_tipo_proyecto}}</td>
-			        </tr>
-				@endforeach
-		    </tbody>
-		</table>
-
-		<div align="center">{!! $tipo_proyectos->render() !!}</div>
-	</div>
+    </div><!-- content -->
 	
-@stop
+</div>
+@endsection
