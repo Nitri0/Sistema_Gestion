f5375b622bb3a8e2638b0e43170992e664f5bdcd
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-12-10 17:39:12 -0430

    text ckeditor crear avance

--- resources/views/avances/create.blade.php
+++ resources/views/avances/create.blade.php
@@ -1,5 +1,9 @@
 @extends('base-admin')
 
+@section('js')
+    <script src="{{ asset('/bower_components/ckeditor/ckeditor.js') }}"></script>
+@endsection
+
 @section('content')
 
 <div id="page-container" class="fade page-sidebar-fixed page-header-fixed" ng-controller="AvanceController">
@@ -22,49 +26,49 @@
 
         <h1 class="page-header"><i class="fa fa-laptop"></i> Crear avance de mi Proyecto </h1>
         
-        <div class="row">
-            <!-- begin col-12 -->
-            <div class="col-12 ui-sortable">
-                <!-- begin panel -->
-                <div class="panel panel-inverse">
-                    <div class="panel-heading">
-                        <div class="panel-heading-btn">
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand" data-original-title="" title=""><i class="fa fa-expand"></i></a>
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload" data-original-title="" title=""><i class="fa fa-repeat"></i></a>
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse" data-original-title="" title=""><i class="fa fa-minus"></i></a>
+        <form class="form-horizontal" action="{{ url('/mis-proyectos/avances/'.$id_proyecto.'/create') }}" method="POST">       
+
+            <div class="row">
+                <!-- begin col-12 -->
+                <div class="col-12 ui-sortable">
+                    <!-- begin panel -->
+                    <div class="panel panel-inverse">
+                        <div class="panel-heading">
+                            <div class="panel-heading-btn">
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand" data-original-title="" title=""><i class="fa fa-expand"></i></a>
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload" data-original-title="" title=""><i class="fa fa-repeat"></i></a>
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse" data-original-title="" title=""><i class="fa fa-minus"></i></a>
+                            </div>
+                            <h4 class="panel-title">Crear avance</h4>
                         </div>
-                        <h4 class="panel-title">Crear avance</h4>
-                    </div>
 
-                    <div class="panel-body">
+                        <div class="panel-body">
 
-                    	<blockquote class="f-s-14">
-                           <p>File Upload widget with multiple file selection, drag&amp;drop support, progress bars, validation and preview images, audio and video for jQuery.<br>
-                            Supports cross-domain, chunked and resumable file uploads and client-side image resizing.<br>
-                            Works with any server-side platform (PHP, Python, Ruby on Rails, Java, Node.js, Go etc.) that supports standard HTML form file uploads.</p>
-                        </blockquote>
-					
-						<div class="well">
+                        	<blockquote class="f-s-14">
+                               <p>File Upload widget with multiple file selection, drag&amp;drop support, progress bars, validation and preview images, audio and video for jQuery.<br>
+                                Supports cross-domain, chunked and resumable file uploads and client-side image resizing.<br>
+                                Works with any server-side platform (PHP, Python, Ruby on Rails, Java, Node.js, Go etc.) that supports standard HTML form file uploads.</p>
+                            </blockquote>
+    					
+    						<div class="well">
 
-							<form class="form-horizontal" action="{{ url('/mis-proyectos/avances/'.$id_proyecto.'/create') }}" method="POST">		
+    							<div class="from-group">
+    								<label class="col-md-4 control-label">Proyecto: </label>
+    								<div class="col-md-5"> 
+    									<h6 class="text-success">{{$proyecto->nombre_proyecto}}</h6>
+    								</div>
+    								<input type="hidden" class="form-control" name="id_proyecto" ng-value='{{$proyecto->id_proyecto}}'>
+    							</div>			
+    		
+    							<div class="from-group">
+    								<label class="col-md-4 control-label">Etapa/Sprint/Paso: </label>
+    								<div class="col-md-5"> 
+    									<h6 class="text-success">{{$proyecto->getEstatus()}}</h6>
+    								</div>
+    								<input type="hidden" class="form-control" name="id_etapa" ng-value='{{$proyecto->getIdEtapa()}}'>
+    							</div>			
 
-								<div class="from-group">
-									<label class="col-md-4 control-label">Proyecto: </label>
-									<div class="col-md-5"> 
-										<h6 class="text-success">{{$proyecto->nombre_proyecto}}</h6>
-									</div>
-									<input type="hidden" class="form-control" name="id_proyecto" ng-value='{{$proyecto->id_proyecto}}'>
-								</div>			
-			
-								<div class="from-group">
-									<label class="col-md-4 control-label">Etapa/Sprint/Paso: </label>
-									<div class="col-md-5"> 
-										<h6 class="text-success">{{$proyecto->getEstatus()}}</h6>
-									</div>
-									<input type="hidden" class="form-control" name="id_etapa" ng-value='{{$proyecto->getIdEtapa()}}'>
-								</div>			
-
-								<div class="form-group">
+    							<div class="form-group">
                                     <label class="col-md-4 control-label">¿Avance de cierre de etapa?</label>
                                     <div class="col-md-5">
                                         <label class="radio-inline">
@@ -79,13 +83,13 @@
                                 </div>
 
                                 <div class="form-group">
-									<label class="col-md-4 control-label"> Asunto </label>
-									<div class="col-md-5">
-										<input type="text" class="form-control" ng-model="avance.notificacion_avance" name="asunto_avance">
-									</div>
-								</div>
+    								<label class="col-md-4 control-label"> Asunto </label>
+    								<div class="col-md-5">
+    									<input type="text" class="form-control" ng-model="avance.notificacion_avance" name="asunto_avance">
+    								</div>
+    							</div>
 
-								<div class="form-group">
+    							<div class="form-group">
                                     <label class="col-md-4 control-label">¿Con copia al cliente?</label>
                                     <div class="col-md-5">
                                         <label class="radio-inline">
@@ -100,43 +104,61 @@
                                 </div>
 
                                 <div class="form-group" ng-if="check==1">
-									<label class="control-label col-md-4">Plantillas</label>
-									<div class="col-md-5">
-									    <select class="form-control js-example-data-array" ng-model="id_plantilla" name ="id_plantilla">
+    								<label class="control-label col-md-4">Plantillas</label>
+    								<div class="col-md-5">
+    								    <select class="form-control js-example-data-array" ng-model="id_plantilla" name ="id_plantilla">
                                             <option value="">Seleccione una plantilla</option>
                                             @foreach($plantillas as $plantilla)
-													<option value="{{$plantilla->id_plantilla}}">
-														{{ $plantilla->nombre_plantilla }}
-													</option>							
-											@endforeach	
+    												<option value="{{$plantilla->id_plantilla}}">
+    													{{ $plantilla->nombre_plantilla }}
+    												</option>							
+    										@endforeach	
                                         </select>
                                     </div>
                                     <div class="col-md-2">
-										<a class="btn btn-sm btn-success" ng-if="id_plantilla" target="_blank" href="{{ url( '/plantillas/preview/'.$proyecto->id_proyecto.'/[[id_plantilla]]/' ) }}"> preview <i class="fa fa-eye"></i></a>
-									</div>
-								</div>
+    									<a class="btn btn-sm btn-success" ng-if="id_plantilla" target="_blank" href="{{ url( '/plantillas/preview/'.$proyecto->id_proyecto.'/[[id_plantilla]]/' ) }}"> preview <i class="fa fa-eye"></i></a>
+    								</div>
+    							</div>
 
-								<div class="form-group">
+    							<!--<div class="form-group">
                                     <label class="col-md-4 control-label">Descripcion</label>
                                     <div class="col-md-5">
                                         <textarea class="form-control" placeholder="Textarea" rows="5" ng-model="avance.descripcion_avance" name="descripcion_avance"></textarea>
                                     </div>
-                                </div>
-							
-								<center>
-									<button class="btn btn-success m-r-5 m-b-5" type="submit">
-											Registrar <i class="fa fa-pencil-square-o"></i>
-									</button>
-								</center>
-
-							</form>
-
-						</div>
-						
-					</div><!-- boby -->
+                                </div>-->
+    							
+    						</div>
+    						
+    					</div><!-- boby -->
+                    </div>
                 </div>
+
+                <div class="col-md-12">
+                    <div class="panel panel-inverse" data-sortable-id="form-wysiwyg-1">
+                        <div class="panel-heading">
+                            <div class="panel-heading-btn">
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                            </div>
+                            <h4 class="panel-title">Descripción</h4>
+                        </div>
+                        <div class="panel-body panel-form">
+                            <textarea class="ckeditor" id="editor1" rows="30" ng-model="avance.descripcion_avance" name="descripcion_avance">
+                            
+                            </textarea>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-        </div>
+        </form>
+        
+        <center>
+            <button class="btn btn-success m-r-5 m-b-5" type="submit">
+                    Registrar <i class="fa fa-pencil-square-o"></i>
+            </button>
+        </center>
 
     </div><!-- content -->
 	
