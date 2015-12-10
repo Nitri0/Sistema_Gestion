93e77a99ec6c03e63e460a501ae507d6a8d99048
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-12-09 22:59:58 -0430

    lib y prueba para ckeditor

--- resources/views/plantillas/create.blade.php
+++ resources/views/plantillas/create.blade.php
@@ -1,5 +1,9 @@
 @extends('base-admin')
 
+@section('js')
+	<script src="{{ asset('/bower_components/ckeditor/ckeditor.js') }}"></script>
+@endsection
+
 @section('content')
 
 <div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
@@ -60,13 +64,13 @@
 		                            <label class="col-md-4 control-label">Data con formato html </label>
 		                        </div>
 
-		                        <div class="form-group">
+		                        <!--<div class="form-group">
 		                        	<div class="col-md-2"></div>
 		                            <div class="col-md-8">
 										<textarea rows="20" class="form-control" ng-model="plantilla.raw_data_plantilla" name="raw_data_plantilla">
 										</textarea>
 		                            </div>
-		                        </div>
+		                        </div>-->
 
 		                    </div>
 
@@ -102,17 +106,41 @@
 									<strong>P.D: no olvidar colocar la etiqueta { !! $data !! } (sin espacios) en el lugar donde estará la data que se llenará automaticamente al crear un avance</strong><br><br><br>
 								</center>
 							</div>
-							
-							<center>
-								<button class="btn btn-success m-r-5 m-b-5"  type="submit">
-									Registrar <i class="fa fa-pencil-square-o"></i>
-								</button>
-							</center>
-						
+								
 						</div><!-- boby -->
 	                </div>
 	            </div>
+
+	            <div class="col-md-12">
+                    <div class="panel panel-inverse" data-sortable-id="form-wysiwyg-1">
+                        <div class="panel-heading">
+                            <div class="panel-heading-btn">
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+                            </div>
+                            <h4 class="panel-title">Data del Correo</h4>
+                        </div>
+                        <div class="panel-body panel-form">
+							<textarea class="ckeditor" id="editor1" rows="20" ng-model="plantilla.raw_data_plantilla" name="raw_data_plantilla">
+							
+							</textarea>
+                        </div>
+                    </div>
+                </div>
+
 	        </div>
+
+	        <p>[[plantilla.raw_data_plantilla]]</p>
+
+	        <center>
+				@if($plantillas)
+				<button type="submit" class="btn btn-danger m-r-5 m-b-5">Actualizar <i class="fa fa-refresh"></i></button>
+				@else
+				<button type="submit" class="btn btn-success m-r-5 m-b-5">Registrar <i class="fa fa-pencil-square-o"></i></button>
+				@endif
+			</center>
         </form>
 
     </div><!-- content -->
