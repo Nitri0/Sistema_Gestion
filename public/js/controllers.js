781f33feb6663ed4292ee215f95d70f88eafc5bd
Homero <hsh283@gmail.com>
2015-12-17 15:43:27 -0430

    agregando validacion a grupo de etapa

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -157,19 +157,45 @@ coreApp.controller('AdminUsuariosController', function ($scope, $log) {
 coreApp.controller('GrupoEtapasController', function ($scope, $log) {
 	console.log("Grupo de etapas");
 	$scope.etapas=[];
-	$scope.cantidad=0;
+	$scope.cantidad_etapas=0;
 	$scope.GrpEtapas={};
 	$scope.submitted = false;
 
 	$scope.agregar_etapa= function(argument) {
 		$scope.etapas.push(1);
-		$scope.cantidad = $scope.etapas.length;
+		$scope.cantidad_etapas = $scope.etapas.length;
 	};
 
 	$scope.eliminar_etapa= function(argument) {
 		$scope.etapas.pop();
-		$scope.cantidad = $scope.etapas.length;
+		$scope.cantidad_etapas = $scope.etapas.length;
 	};
+
+	$scope.submit= function(formValid) {
+		console.log(formValid);
+		$scope.submitted=true;
+		if (formValid==true){
+			if ($scope.cantidad_etapas >0){
+		        var json = {};
+        		angular.element('#formulario').serializeArray().map(function(x){json[x.name] = x.value;});
+
+				$http({
+				    method: 'POST',
+				    url: $scope.urlAction,
+				    data: json,
+				    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				}).then(function successCallback(response) {
+				    $window.location.href = "/proyectos";
+				  }, function errorCallback(response) {
+				  	$window.location.href = "/proyectos";
+				    // called asynchronously if an error occurs
+				    // or server returns response with an error status.
+				  });    		
+			}
+		};
+		return false;
+	}
+	
 });
 
 coreApp.controller('EmpresaController', function ($scope, $log) {
