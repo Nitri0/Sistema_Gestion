c5f4a204bb7e4b2732174ac5a4f88afa25e93ca3
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-01-06 23:11:16 -0430

    list cliente

--- resources/views/clientes/list.blade.php
+++ resources/views/clientes/list.blade.php
@@ -22,78 +22,84 @@
 
         <h1 class="page-header"><i class="fa fa-laptop"></i> Lista de Clientes </h1>
         
+        <div ng-init="models={{$clientes}}"></div>
+		<div ng-init="url='{{url()}}'"></div>
+		
         <div class="row">
-            <!-- begin col-12 -->
-            <div class="col-12 ui-sortable">
-                <!-- begin panel -->
-                <div class="panel panel-inverse">
-                    <div class="panel-heading">
-                        <div class="panel-heading-btn">
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand" data-original-title="" title=""><i class="fa fa-expand"></i></a>
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload" data-original-title="" title=""><i class="fa fa-repeat"></i></a>
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse" data-original-title="" title=""><i class="fa fa-minus"></i></a>
+            <div class="col-12">
+                <div class="panel-group" id="accordion">
+                	<div class="row text-list">
+                		<div class="col-sm-3"> 
+                			<div class="row">
+                				<div class="col-sm-3"># </div>
+                				<div class="col-sm-9">
+                        			Nombre
+                        		</div>
+                			</div>
+                		</div>
+                		<div class="col-sm-3">
+							CI / RIF
+                		</div>
+                		<div class="col-sm-3">
+							Email
+                		</div>
+                		<div class="col-sm-3">
+							Telefono
+                		</div>
+                	</div>
+
+                	<br>
+                    
+                    <div class="panel panel-inverse overflow-hidden custon-list" ng-repeat="model in models| filter:opciones.buscador | orderBy:sort:reverse  track by $index">
+                        <div class="panel-heading">
+                            <h3 class="panel-title list-title">
+                                <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion" href="#[[$index+1]]">
+                                    <i class="fa fa-plus pull-right"></i> 
+                                </a>	
+                            </h3>
+                            <div class="box-button-list">
+		        				<a class="btn btn-sm btn-info btn-cirule" ng-href="{{ url( '/clientes/[[model.id_cliente]]/edit' ) }}" data-toggle="tooltip" data-title="Editar"><i class="fa fa-pencil-square-o"></i></a>
+		        			</div>
+                            <h3 class="panel-title list-title">
+                            	<div class="row">
+                            		<div class="col-sm-3"> 
+                            			<div class="row">
+                            				<div class="col-sm-3"> [[$index+1]] </div>
+                            				<div class="col-sm-9">
+		                            			[[model.nombre_cliente]]
+		                            		</div>
+                            			</div>
+                            		</div>
+
+                            		<div class="col-sm-3">
+										[[model.ci_rif_cliente]]
+                            		</div>
+
+                            		<div class="col-sm-3">
+										[[model.email_cliente]]
+                            		</div>
+
+                            		<div class="col-sm-2">
+										[[model.telefono_cliente]]
+                            		</div>
+
+                            	</div>                           	 
+                            </h3>
                         </div>
-                        <h4 class="panel-title">Clientes</h4>
+                        <div id="[[$index+1]]" class="panel-collapse collapse">
+                            <div class="panel-body">
+                            	<p>Contacto: [[model.persona_contacto_cliente]]</p>
+                            	<p>Dirección: [[model.direccion_cliente]]</p>
+                            	<p>Telefono 2: [[model.telefono_2_cliente]]</p>
+                            	<p>Nombre de Proyecto: [[model.nombre_proyecto]]</p>
+                            	<form action="[[url+'/clientes/'+model.id_cliente]]" method="post">
+					        		<input type="hidden" name="_method" value="delete">
+									<button type="submit" class="btn btn-sm btn-danger pull-right" data-toggle="tooltip" data-title="Eliminar"><i class="fa fa-trash"></i></button>
+								</form>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="panel-body">
-
-						@include('alerts.mensaje_success')
-						@include('alerts.mensaje_error')
-
-
-						<div ng-init="models={{$clientes}}"></div>
-						<div ng-init="url='{{url()}}'"></div>
-
-
-						<table class="table table-striped table-bordered nowrap" width="100%">
-						    <thead>
-						      <tr>
-						        <th>
-						        	<a href="#" ng-click="changeSort('index')">#</a>
-						        </th>		      	
-						        <th>
-						        	<a href="#" ng-click="changeSort('nombre_cliente')">Nombre</a>
-						        </th>
-						        <th>
-						        	<a href="#" ng-click="changeSort('ci_rif_cliente')">CI / RIF</a>
-						        </th>
-						        <th>
-						        	<a href="#" ng-click="changeSort('email_cliente')">Email</a>
-						        </th>
-						        <th>
-						        	<a href="#" ng-click="changeSort('persona_contacto_cliente')">Contacto</a>
-						        </th>
-						        <th>
-						        	<a href="#" ng-click="changeSort('nombre_proyecto')">Proyecto Asociado</a>
-						        </th>		
-						        <th>
-						        	
-						        </th>        
-						      </tr>
-
-						    </thead>
-						    <tbody>
-						    	<tr class="odd gradeX" ng-repeat="model in models| filter:opciones.buscador | orderBy:sort:reverse  track by $index">
-									<td>[[$index]]</td>
-									<td>[[model.nombre_cliente ]]</td>
-									<td>[[model.ci_rif_cliente ]]</td>
-									<td>[[model.email_cliente  ]]</td>
-									<td>[[model.persona_contacto_cliente ]]</td>
-									<td>[[model.nombre_proyecto | noAsignado]]</td>
-						        	<td>
-						        		<form action="[[url+'/clientes/'+model.id_cliente]]" method="post">
-							        		<a class="btn btn-sm btn-info" ng-href="{{ url( '/clientes/[[model.id_cliente]]/edit' ) }}" data-toggle="tooltip" data-title="Editar"><i class="fa fa-pencil-square-o"></i></a>
-							        		<a class="btn btn-sm btn-inverse" ng-href="{{ url( '/clientes/[[model.id_cliente]]') }}" data-toggle="tooltip" data-title="Gestionar"><i class="fa fa-cogs"></i></a>
-											<input type="hidden" name="_method" value="delete">
-											<button type="submit" class="btn btn-sm btn-danger" data-toggle="tooltip" data-title="Eliminar"><i class="fa fa-trash"></i></button>
-										</form>
-						        	</td>
-						        </tr>
-						    </tbody>
-						</table>
-	 
-	 				</div><!-- boby -->
                 </div>
             </div>
         </div>
