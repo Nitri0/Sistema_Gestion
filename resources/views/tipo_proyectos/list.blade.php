8754548e71e9587b70572eedf7129ce82b31c0f1
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-01-09 01:53:37 -0430

    tipo de proyecto list

--- resources/views/tipo_proyectos/list.blade.php
+++ resources/views/tipo_proyectos/list.blade.php
@@ -7,6 +7,9 @@
 	@include('layouts/navbar-admin')
 
     @include('layouts/sidebar-admin')
+
+    @include('alerts.mensaje_error')
+	@include('alerts.mensaje_success')
 	
 	<div id="content" class="content ng-scope">
         
@@ -19,37 +22,58 @@
                 </div>
             </div>
         </ol>
-        
 
-        <h1 class="page-header"><i class="fa fa-sitemap"></i> Tipos de Proyecto </h1>
-		
-		@include('alerts.mensaje_error')
-		@include('alerts.mensaje_success')
+		<h1 class="page-header"></h1>
 
         <div class="row">
-			<!-- begin col-3 -->
-			@foreach($tipo_proyectos as $tipo_proyecto)
-			<div class="col-md-3 col-sm-6">
-				<div class="widget widget-stats bg-blue">
-					<div class="stats-icon"><i class="fa fa-sitemap"></i></div>
-					<div class="stats-info">
-						<h4>PROYECTO</h4>
-						<p>{{$tipo_proyecto->nombre_tipo_proyecto}}</p>	
-					</div>
-					<div class="stats-link">
-						<a href="{{url('/tipo_proyectos/'.$tipo_proyecto->id_tipo_proyecto.'/edit')}}">Editar <i class="fa fa-arrow-circle-o-right"></i></a>
-					</div>
+            <div class="col-12">
+                <div class="panel-group" id="accordion">
+                	<div class="row text-list">
+                		<div class="col-sm-4"> 
+                			<div class="row">
+                				<div class="col-sm-12">
+                        			<i class="fa fa-sitemap"></i> Tipo de Proyecto
+                        		</div>
+                			</div>
+                		</div>
+                	</div>
 
-					<div class="stats-link">
-						<a href="{{url('/tipo_proyectos/'.$tipo_proyecto->id_tipo_proyecto.'/destroy')}}">Eliminar <i class="fa fa-arrow-circle-o-right"></i></a>
-					</div>					
-				</div>
-			</div>
-			@endforeach
-			<!-- end col-3 -->
+                	<br>
+                    @foreach($tipo_proyectos as $tipo_proyecto)
+                    <div class="panel panel-inverse overflow-hidden custon-list">
+                        <div class="panel-heading">
+                            <h3 class="panel-title list-title">
+                                <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion" href="#{{$tipo_proyecto->id_tipo_proyecto}}">
+                                    <i class="fa fa-plus pull-right"></i> 
+                                </a>	
+                            </h3>
+                            <div class="box-button-list">
+		        				<a class="btn btn-sm btn-info btn-cirule" href="{{url('/tipo_proyectos/'.$tipo_proyecto->id_tipo_proyecto.'/edit')}}" data-toggle="tooltip" data-title="Editar"><i class="fa fa-pencil-square-o"></i></a>
+		        			</div>
+                            <h3 class="panel-title list-title">
+                            	<div class="row">
+                            		<div class="col-sm-4"> 
+                            			<div class="row">
+                            				<div class="col-sm-12">
+		                            			{{$tipo_proyecto->nombre_tipo_proyecto}}
+		                            		</div>
+                            			</div>
+                            		</div>
+                            	</div>                           	 
+                            </h3>
+                        </div>
+                        <div id="{{$tipo_proyecto->id_tipo_proyecto}}" class="panel-collapse collapse">
+                            <div class="panel-body">
+								<a href="{{url('/tipo_proyectos/'.$tipo_proyecto->id_tipo_proyecto.'/destroy')}}" class="btn btn-sm btn-danger pull-right" data-toggle="tooltip" data-title="Eliminar"><i class="fa fa-trash"></i></a>
+                        	</div>
+                        </div>
+                    </div>
+                    @endforeach
 
-			<div align="center">{!! $tipo_proyectos->render() !!}</div>
-		</div>
+                </div>
+            </div>
+            <div align="center">{!! $tipo_proyectos->render() !!}</div>
+        </div>
 
     </div><!-- content -->
 	
