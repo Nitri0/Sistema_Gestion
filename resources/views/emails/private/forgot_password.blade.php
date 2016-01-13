ca9cc7863616d3c9d63c3f62cec8dcd5ba22c712
Homero Hernandez <hsh283@gmail.com>
2016-01-13 16:27:17 -0430

    agregando formato al correo

--- resources/views/emails/private/forgot_password.blade.php
+++ resources/views/emails/private/forgot_password.blade.php
@@ -1,12 +1,10 @@
 <p>Hola {{$nombre}},</p>
-<br>
+
 <p>Su nueva contraseña es:</p>
-	<br>
 	<br>
 	<p>{{$password}}</p>
 	<br>
-	<br>
 <p>Para cualquier pregunta o sugerencia, por favor contactenos a {{$contacto_email}}.</p>
-	<br>
+
 <p>Gracias,</p>
 <p>KeyGestion.</p>
