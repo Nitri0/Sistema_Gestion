f5532086a740ff99f7cf01f4f042936351ef27b0
Homero Hernandez <hsh283@gmail.com>
2016-01-13 16:14:29 -0430

    agregando archivos faltantes

---
+++ resources/views/emails/private/forgot_password.blade.php
@@ -0,0 +1,10 @@
+Hi [MM_Member_Data name='firstName'],
+
+Click the link below to reset you account password:
+	
+<a href="[MM_CorePage_Link type='resetpassword']">[MM_CorePage_Link type='resetpassword']</a>
+	
+If you have any questions, please contact us at [MM_Employee_Data name='email'].
+	
+Thanks,
+[MM_Employee_Data name='displayName']
