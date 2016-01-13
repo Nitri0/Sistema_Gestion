b03e75c87a7c608ac4dcefc1a6dd90d8d479534f
Homero Hernandez <hsh283@gmail.com>
2016-01-13 16:27:59 -0430

    agregando mensajes de alertas no error al login

--- resources/views/autenticacion/login.blade.php
+++ resources/views/autenticacion/login.blade.php
@@ -48,7 +48,7 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 						
 						@include('alerts.mensaje_error')
-						@include('alerts.mensaje')
+						@include('alerts.mensaje_success')
 
 	                    <div class="form-group m-b-15">
 	                        <input type="email" class="form-control input-lg" name="correo_usuario" value="{{ old('correo_usuario') }}" placeholder="Correo Electronico" >
