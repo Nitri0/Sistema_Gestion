716c573ac9a8d1cca4b2868a22b9c41e82983866
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-01-14 13:00:31 -0430

    list etapas

--- resources/views/etapas/list.blade.php
+++ resources/views/etapas/list.blade.php
@@ -1,22 +1,5 @@
 @extends('base-admin')
 
-@section('css')
-	<link href="{{ asset('/thema/admin/html/assets/plugins/DataTables/media/css/dataTables.bootstrap.min.css') }}" rel="stylesheet" />
-	<link href="{{ asset('/thema/admin/html/assets/plugins/DataTables/extensions/Responsive/css/responsive.bootstrap.min.css') }}" rel="stylesheet" />
-@endsection
-
-@section('js')
-	<script src="{{ asset('/thema/admin/html/assets/plugins/DataTables/media/js/jquery.dataTables.js') }}"></script>
-	<script src="{{ asset('/thema/admin/html/assets/plugins/DataTables/media/js/dataTables.bootstrap.min.js') }}"></script>
-	<script src="{{ asset('/thema/admin/html/assets/plugins/DataTables/extensions/Responsive/js/dataTables.responsive.min.js') }}"></script>
-	<script src="{{ asset('/thema/admin/html/assets/js/table-manage-responsive.demo.min.js') }}"></script>
-	<script>
-		$(document).ready(function() {
-			TableManageResponsive.init();
-		});
-	</script>
-@endsection
-
 @section('content')
 
 <div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
@@ -24,6 +7,9 @@
 	@include('layouts/navbar-admin')
 
     @include('layouts/sidebar-admin')
+
+    @include('alerts.mensaje_error')
+    @include('alerts.mensaje_success')
 	
 	<div id="content" class="content ng-scope">
         <ol class="breadcrumb pull-right">
@@ -38,53 +24,64 @@
 
         <h1 class="page-header"><i class="fa fa-database"></i> Lista de grupos de etapas</h1>
         
+		<div ng-init="grupoetapas={{$grupo_etapas}}"></div>
+		<div ng-init="url='{{url()}}'"></div>
+
         <div class="row">
-            <!-- begin col-12 -->
-            <div class="col-12 ui-sortable">
-                <!-- begin panel -->
-                <div class="panel panel-inverse">
-                    <div class="panel-heading">
-                        <div class="panel-heading-btn">
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand" data-original-title="" title=""><i class="fa fa-expand"></i></a>
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload" data-original-title="" title=""><i class="fa fa-repeat"></i></a>
-                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse" data-original-title="" title=""><i class="fa fa-minus"></i></a>
+            <div class="col-12">
+                <div class="panel-group" id="accordion">
+                	<div class="row text-list">
+                		<div class="col-sm-5"> 
+                			<div class="row">
+                				<div class="col-sm-3"># </div>
+                				<div class="col-sm-9">
+                        			Nombre de grupo
+                        		</div>
+                			</div>
+                		</div>
+                		<div class="col-sm-3">
+							Cantidad de etapas
+                		</div>
+                	</div>
+
+                	<br>
+                    
+                    <div class="panel panel-inverse overflow-hidden custon-list" ng-repeat="etapa in grupoetapas| filter:opciones.buscador | orderBy:sort:reverse  track by $index">
+                        <div class="panel-heading">
+                            <h3 class="panel-title list-title">
+                                <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion" href="#[[$index+1]]">
+                                    <i class="fa fa-plus pull-right"></i> 
+                                </a>	
+                            </h3>
+                            <div class="box-button-list">
+		        				<a class="btn btn-sm btn-info btn-cirule" ng-href="{{ url( '/grupo_etapas/[[etapa.id_grupo_etapas]]' ) }}" data-toggle="tooltip" data-title="Editar"><i class="fa fa-pencil-square-o"></i></a>
+		        			</div>
+                            <h3 class="panel-title list-title">
+                            	<div class="row">
+                            		<div class="col-sm-5"> 
+                            			<div class="row">
+                            				<div class="col-sm-3"> [[$index+1]] </div>
+                            				<div class="col-sm-9">
+		                            			[[etapa.nombre_grupo_etapas]]
+		                            		</div>
+                            			</div>
+                            		</div>
+
+                            		<div class="col-sm-3">
+										[[etapa.cantidad_etapas]]
+                            		</div>
+                            	</div>                           	 
+                            </h3>
                         </div>
-                        <h4 class="panel-title">Grupos de Etapas</h4>
+                        <div id="[[$index+1]]" class="panel-collapse collapse">
+                            <div class="panel-body">
+                            	<p>Descripción: [[etapa.descripcion_grupo_etapas]]</p>
+                            	
+                            	<a class="btn btn-sm btn-danger pull-right" href="{{ url( '/grupo_etapas/[[etapa.id_grupo_etapas]]/destroy' ) }}" data-toggle="tooltip" data-title="Eliminar"><i class="fa fa-trash"></i></a>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="panel-body">
-
-						@include('alerts.mensaje_error')
-						@include('alerts.mensaje_success')
-
-						<table id="data-table" class="table table-striped table-bordered nowrap" width="100%">
-						    <thead>
-						      <tr>
-						        <th>Nombre de grupo</th>
-						        <th>Descripcion</th>
-						        <th>Cantidad de etapas</th>
-						        <th>Operaciones</th>
-						      </tr>
-						    </thead>
-						    <tbody>
-						    	@foreach($grupo_etapas as $etapa)
-							    	<tr>
-										<td>{{$etapa->nombre_grupo_etapas}}</td>
-							        	<td>{{$etapa->descripcion_grupo_etapas}}</td>
-										<td>{{$etapa->cantidad_etapas}}</td>
-							        	<td>
-							        		<a class="btn btn-sm btn-info" href="{{ url( '/grupo_etapas/'.$etapa->id_grupo_etapas ) }}" data-toggle="tooltip" data-title="Detalle"><i class="fa fa-list"></i></a>
-							        		<a class="btn btn-sm btn-danger" href="{{ url( '/grupo_etapas/'.$etapa->id_grupo_etapas.'/destroy' ) }}" data-toggle="tooltip" data-title="Eliminar"><i class="fa fa-trash"></i></a>
-							        	</td>
-							        </tr>
-								@endforeach
-						    </tbody>
-						</table>
-
-						<div align="center">{!! $grupo_etapas->render() !!}</div>
-
-	
-					</div><!-- boby -->
                 </div>
             </div>
         </div>
