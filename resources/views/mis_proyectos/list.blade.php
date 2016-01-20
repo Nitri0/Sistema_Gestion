85e798ee6e0bc24fe2d399d5dfdfc71e28a0f18e
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-01-20 18:43:54 -0430

    fecha

--- resources/views/mis_proyectos/list.blade.php
+++ resources/views/mis_proyectos/list.blade.php
@@ -95,12 +95,12 @@
                                         [[proyecto.nombre_dominio | noAsignado ]]
                                     </a>
                                 </p>
-                                <p>Fecha de creación: [[proyecto.DATE_FORMAT(t_proyectos.fecha_creacion_proyecto,'%d-%m-%Y')]] </p>
+                                <p>Fecha de creación: [[proyecto.fecha_creacion_proyecto]] </p>
                                 <p>Rol: [[proyecto.nombre_tipo_rol]]</p>
                                 <p>Tipo de Proyecto: [[proyecto.nombre_grupo_etapas]]</p>
                                 <hr>
                                 <p class="center">Ultimo Avance</p>
-                                <div class="row" ng-if="proyecto.asunto_avance.length != 0">
+                                <div class="row" ng-if="proyecto.asunto_avance.length == 0">
                                     <div class="col-md-8 col-md-offset-2">
                                         <ul class="chats">
                                             <li class="left">
