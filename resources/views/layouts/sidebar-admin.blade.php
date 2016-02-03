a0b4d02d41659ba7eb298c4ec58c3702695079e7
Homero Hernandez <hsh283@gmail.com>
2016-02-03 13:28:59 -0430

    agregando validacion en modulo web

--- resources/views/layouts/sidebar-admin.blade.php
+++ resources/views/layouts/sidebar-admin.blade.php
@@ -109,7 +109,7 @@
 				</ul>
 			</li>
 			@endif
-			@if(Auth::user()->getIdEmpresa() == 1 )
+			@if(Auth::user()->getIdEmpresa() == 1 && (Auth::user()->tiene_permiso('dominios') || Auth::user()->tiene_permiso('empresas_proveedoras')  ))
 			<li class="has-sub"><!-- ng-click="proyecto_active()" ng-class="{'': !proyecto, 'active': proyecto}"-->
 				<a href="javascript:;" >
 				    <b class="caret pull-right"></b>
