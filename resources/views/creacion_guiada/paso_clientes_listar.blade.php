d6aca56d074703c2ddb7a84d6f70b96addac8518
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-02-02 19:49:20 -0430

    colappse borrado en list wizards

--- resources/views/creacion_guiada/paso_clientes_listar.blade.php
+++ resources/views/creacion_guiada/paso_clientes_listar.blade.php
@@ -67,11 +67,11 @@
                     
                     <div class="panel panel-inverse overflow-hidden custon-list" ng-repeat="model in models| filter:opciones.buscador | orderBy:sort:reverse  track by $index">
                         <div class="panel-heading">
-                            <h3 class="panel-title list-title">
+                            <!--<h3 class="panel-title list-title">
                                 <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion" href="#[[$index+1]]">
                                     <i class="fa fa-plus pull-right"></i> 
                                 </a>	
-                            </h3>
+                            </h3>-->
                             <!--<div class="box-button-list">
 		        				<a class="btn btn-list" ng-href="{{ url( '/clientes/[[model.id_cliente]]/edit' ) }}" data-toggle="tooltip" data-title="Editar"><i class="fa fa-pencil-square-o"></i></a>
 		        			</div>-->
