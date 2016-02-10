81be639efb823063d31b0915529a609617c3e9ec
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-02-09 21:47:44 -0430

    tipo de proyecto ayuda

---
+++ resources/views/modals/ayudas/tipo-proyecto.blade.php
@@ -0,0 +1,70 @@
+<div class="modal fade bs-example-modal-lg" id="tipo-proyecto-ayuda" labelledby="myLargeModalLabel">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h4 class="modal-title">Creación Tipo de Proyecto</h4>
+			</div>
+			<div class="modal-body modal-body-ayuda">
+                <section id="slider-ayuda">
+					<div class="container-ayuda-slider">
+						<div class="row">
+							<div class="col-sm-12">
+								<div id="slider-carousel" class="carousel slide" data-ride="carousel">
+									<ol class="carousel-indicators carousel-indicators-ayuda">
+										<li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+										<li data-target="#slider-carousel" data-slide-to="1"></li>
+										<li data-target="#slider-carousel" data-slide-to="2"></li>
+										<li data-target="#slider-carousel" data-slide-to="3"></li>
+									</ol>
+									<div class="carousel-inner">		
+										<div class="item item-ayuda center active">
+											<div class="col-sm-12">
+												<p style="color:#B4B1AB;"><strong>¿Que son Tipo de Proyectos?</strong></p>
+												<p>Son los pasos a seguir o procesos a desarrollar de un determinado proyecto, el cual puede estar definido de manera estricta o flexible. Ayudando a crear inducción de manera directa o indirecta a tu equipo de trabajo, evitando retrasos por el desconocimiento de su ejecución.</p>
+												<div class="img-ayuda">
+													<img src="{{ asset('img/ayudas/tipo-proyecto/1.png') }}" class="center img-ayuda-slider" alt="" />
+												</div>
+											</div>
+										</div>
+										<div class="item item-ayuda">
+											<div class="col-sm-6 col-left">
+												<img src="{{ asset('img/ayudas/tipo-proyecto/2.png') }}" class="girl img-responsive" alt="" />
+											</div>
+											<div class="col-sm-6">
+												<h1>Sección informativa del tipo de proyecto</h1>
+												<p>Describe de manera precisa el tipo de proyecto a desarrollar, de tal manera que te permita identificarlo entre la cantidad de proyecto que manejaras a través del tiempo.</p>
+											</div>
+										</div>
+										<div class="item item-ayuda">
+											<div class="col-sm-6">
+												<h1>¿Como agregar nueva etapa?</h1>
+												<p>Presionando el botón de agregar, el sistema le desplegará las opciones de manera dinámica para definir el nombre de la etapa correspondiente enumeradas de manera ascendente.</p>
+											</div>
+											<div class="col-sm-6 col-right">
+												<img src="{{ asset('img/ayudas/tipo-proyecto/3.png') }}" class="girl img-responsive" alt="" />
+											</div>
+										</div>
+										<div class="item item-ayuda center">
+											<div class="col-sm-12">
+												<p style="color:#B4B1AB;"><strong>Registrar Tipo de Proyecto</strong></p>
+												<p>Si estas completamente seguro de la definición de proceso, podrás salvar los cambios para utilizarlo posteriormente. Es importante estar seguro ya que por medidas de seguridad no podrás agregar o eliminar etapas del proyecto.</p>
+												<br>
+												<div class="img-ayuda">
+													<img src="{{ asset('img/ayudas/tipo-proyecto/4.png') }}" class="center img-ayuda-slider" alt="" />
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>		
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+			<div class="modal-footer">
+				<a href="javascript:;" class="btn btn-sm btn-danger" data-dismiss="modal">Cerrar</a>
+			</div>
+		</div>
+	</div>
+</div>
