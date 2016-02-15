176f9ce46d2d1c361b575aa9aac053ce7e8b4a33
Homero <hsh283@gmail.com>
2016-02-15 13:28:45 -0430

    corrigiendo bug de eliminacion de plantillas

--- app/Http/Controllers/PlantillasController.php
+++ app/Http/Controllers/PlantillasController.php
@@ -118,11 +118,11 @@ class PlantillasController extends Controller {
 	public function destroy($id){
 		$ruta = realpath(dirname("../resources/views/emails/."));
 		if (!$this->plantillas->nombre_archivo_plantilla){
-			if (!file_exists($ruta."/".$this->plantillas->nombre_plantilla.".blade.php")){
+			if (file_exists($ruta."/".$this->plantillas->nombre_plantilla.".blade.php")){
 				unlink($ruta."/".$this->plantillas->nombre_plantilla.".blade.php");
 			}
 		}else{
-			if (!file_exists($ruta."/".$this->plantillas->nombre_archivo_plantilla.".blade.php")){
+			if (file_exists($ruta."/".$this->plantillas->nombre_archivo_plantilla.".blade.php")){
 				unlink($ruta."/".$this->plantillas->nombre_archivo_plantilla.".blade.php");
 			}
 		}
