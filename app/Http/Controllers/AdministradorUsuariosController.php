5c3dd5a159d1397cda789b156319de404ec483d1
Homero Hernandez <hsh283@gmail.com>
2016-02-16 14:40:00 -0430

    quitando comprobacion en base

--- app/Http/Controllers/AdministradorUsuariosController.php
+++ app/Http/Controllers/AdministradorUsuariosController.php
@@ -120,7 +120,7 @@ class AdministradorUsuariosController extends Controller
                         ->get()
                         ->count();
         if (!$user->tieneSuscripcion() || $cantidad_usuarios >= $user->cantidad_usuarios){
-            Session::flash('upgrade','upgrade');
+            Session::flash('upgrade','prueba');
             return redirect('/admin_usuarios');
         }
 
