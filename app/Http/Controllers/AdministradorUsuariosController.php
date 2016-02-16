7a9e4e18e4ad24ed567add6c660e6c3adc8d4d44
Homero Hernandez <hsh283@gmail.com>
2016-02-16 15:00:34 -0430

    agregando mensajes de prueba

--- app/Http/Controllers/AdministradorUsuariosController.php
+++ app/Http/Controllers/AdministradorUsuariosController.php
@@ -116,9 +116,7 @@ class AdministradorUsuariosController extends Controller
 
     public function create(){
         $user = Auth::user();
-        $cantidad_usuarios = MMEmpresasUsuarios::where('id_empresa', Auth::user()->getIdEmpresa())
-                        ->get()
-                        ->count();
+        dd($user->puedeAgregarUsuarios());
         if ($user->puedeAgregarUsuarios()){
             Session::flash('upgrade','prueba');
             return redirect('/admin_usuarios');
