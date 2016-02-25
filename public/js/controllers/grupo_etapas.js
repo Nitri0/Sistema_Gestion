26dbf503f86c87928848b0be29c086fdc942fe56
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-02-25 15:21:54 -0430

    grupo de etapa o tipo de proyecto

--- public/js/controllers/grupo_etapas.js
+++ public/js/controllers/grupo_etapas.js
@@ -65,4 +65,60 @@ coreApp.controller('GrupoEtapasController', function ($scope, $log, $http, $wind
 		console.log(url_eliminar);
 		$scope.eliminar_url = url_eliminar;
 	};	
+
+	$scope.tour_ayuda = function(){
+		console.log("init TourController");
+		var tour = new Tour({
+		  	steps: [
+				  {
+				    element: "#tipo_proyecto",
+				    title: "Cliente",
+				    content: "Aquí podrá encontrar las opciones relacionadas a notificaciones, opciones de usuarios, búsqueda y mucho más...",
+				    placement: "bottom",
+				    backdrop: true,
+				  },
+				  {
+				    element: "#descripcion_tipo_proyecto",
+				    title: "Configuración",
+				    content: "Aquí podrás realizar configuraciones de tu cuenta y compartir con tus amigos.",
+				    placement: "left",
+				    backdrop: true,
+				  },
+				  {
+				    element: "#agregar_etapas",
+				    title: "Configuración",
+				    content: "Aquí podrás realizar configuraciones de tu cuenta y compartir con tus amigos.",
+				    placement: "left",
+				    backdrop: true,
+				  },
+				  {
+				    element: "#agregar_etapa",
+				    title: "Buscador",
+				    content: "Aquí podrás realzar la búsqueda relacionada a la pantalla donde estes ubicado.",
+				    placement: "right",
+				    backdrop: true,
+				  }
+				],
+			storage: false,
+			template: "<div class='popover tour'>"+
+					    "<div class='arrow'></div>"+
+					    "<h3 class='popover-title'></h3><button class='btn btn-link btn-cerrar-paseo' data-role='end'><i class='fa fa-times'></i></button>"+
+					    "<div class='popover-content'></div>"+
+					    "<div class='popover-navigation'>"+
+					        "<button class='btn btn-link' data-role='prev'>« Atras</button>"+
+					        "<span data-role='separator'>|</span>"+
+					        "<button class='btn btn-link' data-role='next'>Siguiente »</button>"+
+					    "</div>"+
+					    
+					    "</nav>"+
+					  "</div>"
+
+		});
+
+		// Initialize the tour
+		tour.init(true);
+
+		// Start the tour
+		tour.start(true);
+	};
 });
