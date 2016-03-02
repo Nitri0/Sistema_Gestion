7c1b4001b5fae1d7f4c8a855aa040789f1b8a97a
Diego Carciente <diego.carciente@gmail.com>
2016-03-02 22:16:13 +0100

    Modificado modal de upgrade

--- resources/views/modals/upgrade-cuenta.blade.php
+++ resources/views/modals/upgrade-cuenta.blade.php
@@ -4,16 +4,16 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-				<h4 class="modal-title">Lo Sentimos <i class="fa fa-exclamation"></i></h4>
+				<h4 class="modal-title" align="center">Lo Sentimos... Rellene el siguiente formulario</h4>
 			</div>
 			<div ng-init="urlRedirect='{{url('/admin_usuarios')}}'"></div>
 			<div class="modal-body" align="center" ng-init="urlAction='{{url('/contactame')}}'">
-				<p>Para agregar mas personas a tu equipo de trabajo debes comunicarte con el equipo de soporte llenando el siguiente formulario.</p>
+				<p>Formulario para solicitar información de planes y servicios.</p>
 			</div>
 			<form class="form-horizontal" name="formulario" id="formulario" action="[[urlAction]]" method="POST">	
 				<div class="form-group">
-				    <label class="col-md-4 control-label">Nombres *</label>
-				    <div class="col-md-5">
+				    <label class="col-md-4 control-label">Nombres</label>
+				    <div class="col-md-6">
 				       <input type="text" text-num-only class="form-control" name="nombres" ng-model="nombres" ng-required="true" oninvalid="setCustomValidity(' ')">
 						<div class="error campo-requerido" ng-show="formulario.nombres.$invalid && (formulario.nombres.$touched || submitted)">
 				            <small class="error" ng-show="formulario.nombres.$error.required">
@@ -24,8 +24,8 @@
 				</div>
 
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Teléfono *</label>
-                    <div class="col-md-5" >
+                    <label class="col-md-4 control-label">Teléfono</label>
+                    <div class="col-md-6" >
                        <input type="text" text-num-only class="form-control" name="telefono" ng-model="telefono" ng-required="true" oninvalid="setCustomValidity(' ')">
 						<div class="error campo-requerido" ng-show="formulario.telefono.$invalid && (formulario.telefono.$touched || submitted)">
                             <small class="error" ng-show="formulario.telefono.$error.required">
@@ -36,8 +36,8 @@
                 </div>
 
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Correo *</label>
-                    <div class="col-md-5" >
+                    <label class="col-md-4 control-label">Correo</label>
+                    <div class="col-md-6" >
                        <input type="text" text-num-only class="form-control" name="correo" ng-model="correo" ng-required="true" oninvalid="setCustomValidity(' ')">
 						<div class="error campo-requerido" ng-show="formulario.correo.$invalid && (formulario.correo.$touched || submitted)">
                             <small class="error" ng-show="formulario.correo.$error.required">
@@ -49,8 +49,8 @@
 
                 <div class="form-group">
                     <label class="col-md-4 control-label">Comentario</label>
-                    <div class="col-md-5" >
-                       <input type="text" text-num-only class="form-control" name="comentario" ng-model="comentario" ng-required="true" oninvalid="setCustomValidity(' ')">
+                    <div class="col-md-6" >
+                       <textarea class="form-control" name="comentario" ng-model="comentario" ng-required="true" oninvalid="setCustomValidity(' ')" rows="5"></textarea>
 						<div class="error campo-requerido" ng-show="formulario.correo.$invalid && (formulario.correo.$touched || submitted)">
                             <small class="error" ng-show="formulario.correo.$error.required">
                                 * Campo requerido.
